071e43d22cc38a233efd17c71fc8fd831bc2fb19
Aldrich23xX <39071289+iAldrich23xX@users.noreply.github.com>
2025-06-05 09:55:16 -0500

    Add button to up page

--- js/app.js
+++ js/app.js
@@ -249,6 +249,16 @@ document.addEventListener("DOMContentLoaded", () => {
                         }
                     });
                 }
+
+                const btn = document.getElementById("btnVolverArriba");
+
+                window.addEventListener("scroll", () => {
+                    btn.style.display = (window.scrollY > 200) ? "block" : "none";
+                });
+
+                btn.addEventListener("click", () => {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                });
             });
     }
 });
